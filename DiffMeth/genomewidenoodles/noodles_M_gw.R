3eca7624e6bae7990d32e6de0b0d635d61f3e8fe
Alexander Favorov <favorov@sensi.org>
2014-05-16 04:37:43 -0400

    nooodle+coverage work!!!

--- DiffMeth/genomewidenoodles/noodles_M_gw.R
+++ DiffMeth/genomewidenoodles/noodles_M_gw.R
@@ -6,11 +6,36 @@
 		biocLite("devtools")
 		library("devtools")
 	}
-	install_github('Differential.Coverage','favorov')
+	#install_github('Differential.Coverage','favorov')
+	load_all('../../../../differential.coverage/')
 	library('Differential.Coverage')
 }
 
-stop()
+noodles.M.loaded<-FALSE
+# we can the whole thing to noodles.1000.Rda
+if(file.exists('noodles.M.Rda'))
+{
+	loaded<-load('noodles.M.Rda')
+	if ('noodles.M.methylation' %in% loaded) 
+		if (class(noodles.M.methylation)=='data.frame')
+			if ('noodles.M' %in% loaded)
+				if(class(noodles.M)=='GRanges')
+			noodles.M.loaded<-TRUE
+}
+if(!noodles.M.loaded)
+{
+	beddir<-'../../../bedfiles/'
+	noodle.length<-1000
+	chrs<-nucl.chromosomes.hg19()
+	noodles.M<-prepare.uniform.noodles(chrs,noodle.length)
+	bedfiles<-dir(beddir) 
+	bedfiles<-bedfiles[grep('All_',bedfiles,invert=TRUE)] # remove two 'All_' files
+	bed.ids<-sapply(strsplit(bedfiles,split='_'),function(x){if(x[2]!='DNA') x[2] else x[3]}) #somhere id in pos 2, somewhere in 3
+	noodles.M.methylation<-CountCoverageOfNoodles(noodles.M,paste0(beddir,bedfiles),bed.ids)
+	save(file='noodles.M.Rda',list=c('noodles.M','noodles.M.methylation','bed.ids','noodle.length'))
+}
+
+stop('OK')
 
 if (!suppressWarnings(require('DASiR')))
 {
