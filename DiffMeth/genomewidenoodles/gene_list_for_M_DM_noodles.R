f35660ee374859aaa0fdcd6eef445d2e3786d712
Alexander Favorov <favorov@sensi.org>
2014-06-04 03:08:00 -0400

    giong on

--- DiffMeth/genomewidenoodles/gene_list_for_M_DM_noodles.R
+++ DiffMeth/genomewidenoodles/gene_list_for_M_DM_noodles.R
@@ -1,3 +1,16 @@
+if (!suppressWarnings(require('Differential.Coverage')))
+{
+	if (!suppressWarnings(require('devtools')))
+	{
+		source("http://bioconductor.org/biocLite.R")
+		biocLite("devtools")
+		library("devtools")
+	}
+	install_github('Differential.Coverage','favorov')
+	#load_all('../../../../differential.coverage/')
+	library('Differential.Coverage')
+}
+
 if (!require('Homo.sapiens'))
 {
   source("http://bioconductor.org/biocLite.R")
@@ -10,6 +23,8 @@ if (!require('org.Hs.eg.db'))
   biocLite('org.Hs.eg.db')
   library('org.Hs.eg.db')  
 }
+
+flanks<-10000
 
 noodles.M.loaded<-FALSE
 # we can the whole thing to noodles.M.Rda
@@ -38,4 +53,34 @@ if(!noodles.M.fisher.results.loaded)
 	source('noodles_M_gw.R')
 }
 
+message('all loaded')
+
+DM.M.noodles.indices<-which(fisher.p.values*tests.number<0.05)
+DM.M.noodles<-noodles.M[DM.M.noodles.indices]
+DM.M.noodles$p.value<-fisher.p.values[DM.M.noodles.indices]
+DM.M.noodles$ishyper<-CI_95_L[DM.M.noodles.indices]>1
+
+expanded.DM.M.noodles<-DM.M.noodles
+#inflate DM noodles
+start(expanded.DM.M.noodles)<-pmax(0,start(DM.M.noodles)-flanks)
+end(expanded.DM.M.noodles)<-pmin(end(DM.M.noodles)+flanks,as.integer(seqlengths(DM.M.noodles)[as.character(seqnames(DM.M.noodles))]))
+
+#prepare gene TSS
+TSS<- genes(TxDb.Hsapiens.UCSC.hg19.knownGene)
+
+geneSymbols <- select(
+	org.Hs.eg.db,
+	keys=as.character(TSS$gene_id),
+	columns=c('SYMBOL'),
+	keytype='ENTREZID'
+)
+
+TSS$SYMBOL <- geneSymbols$SYMBOL
+
+tss.start<-ifelse(strand(TSS)=='+',start(TSS),end(TSS))
+
+start(TSS)<-tss.start
+end(TSS)<-tss.start
+
+#make overlap
 
