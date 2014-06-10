f29f746fb334f0f008190c54e2c209f817a63751
Alexander Favorov <favorov@sensi.org>
2014-06-09 23:27:09 -0400

    working nonparallel fisher.gw.noodles.M.R

--- DiffMeth/genomewidenoodles/fisher.gw.noodles.M.R
+++ DiffMeth/genomewidenoodles/fisher.gw.noodles.M.R
@@ -1,16 +1,3 @@
-if (!suppressWarnings(require('Differential.Coverage')))
-{
-	if (!suppressWarnings(require('devtools')))
-	{
-		source("http://bioconductor.org/biocLite.R")
-		biocLite("devtools")
-		library("devtools")
-	}
-	install_github('Differential.Coverage','favorov')
-	#load_all('../../../../differential.coverage/')
-	library('Differential.Coverage')
-}
-
 noodles.M.loaded<-FALSE
 # we can the whole thing to noodles.M.Rda
 if(file.exists('noodles.M.Rda'))
@@ -35,44 +22,30 @@ if(file.exists('noodles.M.fisher.results.Rda'))
 
 if(!noodles.M.fisher.results.loaded)
 {
+	#noodles.M.methylation=noodles.M.methylation[1:60000,] #test
 	message('fishering')
 
 	contrast<-logical(length(bed.ids))
 	contrast[grep('HN',bed.ids)]<-TRUE
 
-	tests.number<-length(noodles.M)
-	fisher.p.values<-numeric(tests.number)
-	meth.in.normals.ratio<-numeric(tests.number)
-	meth.in.tumors.ratio<-numeric(tests.number)
-	OR<-numeric(tests.number)
-	CI_95_L<-numeric(tests.number)
-	CI_95_H<-numeric(tests.number)
+	tests.number<-dim(noodles.M.methylation)[1]
 
+	fisher.noodles.M.result<-data.frame('fisher.p.values'=numeric(tests.number),'meth.in.normals.ratio'=numeric(tests.number),'meth.in.tumors.ratio'=numeric(tests.number),
+		'OR'=numeric(tests.number),'CI_95_L'=numeric(tests.number),'CI_95_H'=numeric(tests.number))
 
-	for (rown in 1:tests.number) %dopar%
+	for (rown in 1:tests.number) 	
 	{
 		cotable<-table(as.logical(noodles.M.methylation[rown,]),contrast)
 		if(nrow(cotable)==1)#nonmeth
 		{
-			fisher.p.values[rown]<-1.
-			meth.in.tumors.ratio[rown]<-0
-			meth.in.normals.ratio[rown]<-0
-			OR[rown]<-NA
-			CI_95_L[rown]<-NA
-			CI_95_H[rown]<-NA
+			fisher.noodles.M.result[rown,]<-c(1,0,0,NA,NA,NA)
 			next
 		}
 		fisherres<-fisher.test(cotable)
-		fisher.p.values[rown]<-fisherres$p.value
-		meth.in.tumors.ratio[rown]<-cotable[2,2]/cotable[1,2]
-		meth.in.normals.ratio[rown]<-cotable[2,1]/cotable[1,1]
-		OR[rown]<-fisherres$estimate
-		CI_95_L[rown]<-fisherres$conf.int[1]
-		CI_95_H[rown]<-fisherres$conf.int[2]
+		fisher.noodles.M.result[rown,]<-c(fisherres$p.value,cotable[2,2]/cotable[1,2],cotable[2,1]/cotable[1,1],fisherres$estimate,fisherres$conf.int[1],fisherres$conf.int[2])
 	}
-	stopCluster(clust)
 	message('done\n')
 	message('Saving...\n')
-	save(file='noodles.M.fisher.results.Rda',list=c('fisher.p.values','tests.number','contrast','OR','CI_95_L','CI_95_H'))
+	save(file='noodles.M.fisher.results.Rda',list=c('fisher.noodles.M.result','tests.number','contrast'))
 }
 
