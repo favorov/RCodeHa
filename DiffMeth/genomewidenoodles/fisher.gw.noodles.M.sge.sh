621072b77e27fbb9f62d832577e4cc6fa6cece18
Alexander Favorov <favorov@sensi.org>
2014-06-15 03:02:19 -0400

    sh script for sge, first completed version

--- DiffMeth/genomewidenoodles/fisher.gw.noodles.M.sge.sh
+++ DiffMeth/genomewidenoodles/fisher.gw.noodles.M.sge.sh
@@ -1,3 +1,4 @@
 #/usr/bin/bash
-workers=20
-workid=qsub -t 1-$workers fisher.dw.noodles.M.sge.run.worker.sh
+workers=30
+echo "Rscript qsub fisher.gw.noodles.M.sge.R worker $workers" > qsub -cwd -t 1-$workers -N "fisher.gw.noodles.M.sge.workers_array_$workers"
+echo "Rscript qsub fisher.gw.noodles.M.sge.R combiner $workers" > qsub -cwd -hold_jid "fisher.gw.noodles.M.sge.workers_array_$workers" -N "fisher.gw.noodles.M.sge.combiner_$workers"
