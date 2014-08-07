174ddd623c792e8f09f6d2355ad107784cb2a1a8
Alexander Favorov <favorov@sensi.org>
2014-08-07 20:41:47 +0300

    prepating worker test

--- DiffMeth/genomewidenoodles/M/fisher.gw.noodles.M.10.workers.sh
+++ DiffMeth/genomewidenoodles/M/fisher.gw.noodles.M.10.workers.sh
@@ -1,8 +1,8 @@
 #/usr/bin/bash
-workers=10
-for w in 1:10 
+workers=100
+for ((w=1; w<=$workers; w++)) 
 do
-	echo "Rscript fisher.gw.noodles.M.sge.R worker $workers"
-	echo "Rscript fisher.gw.noodles.M.sge.R worker $w $workers" | qsub -cwd -N "fisher.gw.noodles.M.sge.workers_array_$w_$workers"
+	echo "Rscript fisher.gw.noodles.M.sge.R worker $w $workers"
+	#echo "Rscript fisher.gw.noodles.M.sge.R worker $w $workers" | qsub -cwd -N "fisher.gw.noodles.M.sge.workers_array_$w_$workers"
 done
 #echo "Rscript qsub fisher.gw.noodles.M.sge.R combiner $workers" > qsub -cwd -hold_jid "fisher.gw.noodles.M.sge.workers_array_$workers" -N "fisher.gw.noodles.M.sge.combiner_$workers"
