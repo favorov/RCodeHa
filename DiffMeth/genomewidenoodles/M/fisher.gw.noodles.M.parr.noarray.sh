3f22fdc1bac059d00dfcb383eb454115cc58ba9e
Alexander Favorov <favorov@sensi.org>
2014-08-14 06:31:05 +0400

    adjusting scripts

--- DiffMeth/genomewidenoodles/M/fisher.gw.noodles.M.parr.noarray.sh
+++ DiffMeth/genomewidenoodles/M/fisher.gw.noodles.M.parr.noarray.sh
@@ -3,6 +3,6 @@ workers=50
 for ((w=1; w<=$workers; w++)) 
 do
 	echo "Rscript fisher.gw.noodles.M.sge.R worker $w $workers"
-	echo "Rscript fisher.gw.noodles.M.sge.R worker $w $workers" | qsub -cwd -N "fisher.gw.noodles.M.sge.workers_$w_$workers"
+	echo "Rscript fisher.gw.noodles.M.sge.R worker $w $workers" | qsub -cwd -N "fisher.gw.noodles.M.sge.workers.$w.$workers"
 done
 Rscript qsub fisher.gw.noodles.M.sge.R combiner $workers
--- DiffMeth/genomewidenoodles/M/fisher.gw.noodles.M.sge.sh
+++
@@ -1,4 +0,0 @@
-#/usr/bin/bash
-workers=30
-echo "Rscript qsub fisher.gw.noodles.M.sge.R worker $workers" > qsub -cwd -t 1-$workers -N "fisher.gw.noodles.M.sge.workers_array_$workers"
-echo "Rscript qsub fisher.gw.noodles.M.sge.R combiner $workers" > qsub -cwd -hold_jid "fisher.gw.noodles.M.sge.workers_array_$workers" -N "fisher.gw.noodles.M.sge.combiner_$workers"
