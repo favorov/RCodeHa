7261ca46374dacc3953ea1570890f4a543a2bf23
Alexander Favorov <favorov@sensi.org>
2014-08-14 06:16:00 +0400

    script renamed

--- DiffMeth/genomewidenoodles/M/fisher.gw.noodles.M.parr.noarray.sh
+++ DiffMeth/genomewidenoodles/M/fisher.gw.noodles.M.parr.noarray.sh
@@ -5,4 +5,4 @@ do
 	echo "Rscript fisher.gw.noodles.M.sge.R worker $w $workers"
 	echo "Rscript fisher.gw.noodles.M.sge.R worker $w $workers" | qsub -cwd -N "fisher.gw.noodles.M.sge.workers_$w_$workers"
 done
-#echo "Rscript qsub fisher.gw.noodles.M.sge.R combiner $workers" > qsub -cwd -hold_jid "fisher.gw.noodles.M.sge.workers_array_$workers" -N "fisher.gw.noodles.M.sge.combiner_$workers"
+Rscript qsub fisher.gw.noodles.M.sge.R combiner $workers
