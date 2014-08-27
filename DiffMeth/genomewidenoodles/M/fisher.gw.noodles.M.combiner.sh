f01f094543203ae2d2177e8db9de436d04b7476d
Alexander Favorov <favorov@sensi.org>
2014-08-27 15:53:12 +0400

    combiner started

---
+++ DiffMeth/genomewidenoodles/M/fisher.gw.noodles.M.combiner.sh
@@ -0,0 +1,9 @@
+#/usr/bin/bash
+workers=50
+Rscript fisher.gw.noodles.M.sge.R combiner $workers || exit 1
+echo 'cleaning...'
+#for ((w=1; w<=$workers; w++)) 
+#do
+#	rm fisher.gw.noodles.M.sge.workers.$w.$workers.e*?
+#	rm fisher.gw.noodles.M.sge.workers.$w.$workers.o*?
+#done
