e3c1bbbc079968f80c98984abbf56e5123baa725
Alexander Favorov <favorov@sensi.org>
2014-10-29 23:41:45 -0400

    cleaner script moved to other file

---
+++ DiffMeth/genomewidenoodles/M/fisher.gw.noodles.M.cleaner.sh
@@ -0,0 +1,2 @@
+#/usr/bin/bash
+rm fisher.gw.noodles.M.worker.*
--- DiffMeth/genomewidenoodles/M/fisher.gw.noodles.M.combiner.sh
+++ DiffMeth/genomewidenoodles/M/fisher.gw.noodles.M.combiner.sh
@@ -1,9 +1,3 @@
 #/usr/bin/bash
 workers=50
 Rscript fisher.gw.noodles.M.R combiner $workers || exit 1
-echo 'cleaning...'
-#for ((w=1; w<=$workers; w++)) 
-#do
-#	rm fisher.gw.noodles.M.sge.workers.$w.$workers.e*?
-#	rm fisher.gw.noodles.M.sge.workers.$w.$workers.o*?
-#done
