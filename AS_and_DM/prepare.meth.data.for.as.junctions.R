f2344342a3eafbd6f44b723c76e948465cf75e10
Alexander Favorov <favorov@sensi.org>
2016-09-20 04:21:31 -0400

    now, prepare noodles also

--- AS_and_DM/prepare.meth.data.for.as.junctions.R
+++ AS_and_DM/prepare.meth.data.for.as.junctions.R
@@ -69,5 +69,59 @@ if(!as.and.dm.tables.loaded)
 	mcols(list.by.gene)<-data.frame(gene=names(list.by.gene))
 	as.junction.ranges<-unlist(list.by.gene)
 	mbd.as.junction.coverage<-count.coverage.of.noodles(as.junction.ranges,paste(mbd.bed.dir,mbd.bed.files,sep='/'),mbd.bed.ids)
-	save(file='as.and.dm.tables.Rda',list=c('as.junction.ranges','mbd.as.junction.coverage'))
+	as.genes.symbols<-names(junctionRanks)
+	save(file='as.and.dm.tables.Rda',list=c('as.junction.ranges','mbd.as.junction.coverage','as.genes.symbols'))
 }
+
+noodles.as.genes.tables.loaded<-FALSE
+if ('mbd.as.genes.noodles.coverage' %in% ls() &&
+'as.genes.noodles.ranges' %in% ls() &&
+class(as.genes.noodles.ranges)=='GRanges' &&	
+(
+	class(mbd.as.genes.noodles.coverage)=='dgCMatrix' || 
+	class(mbd.as.genes.noodles.coverage)=='matrix')
+)	noodles.as.genes.tables.loaded <-TRUE
+#if they are already in the space, do nothing
+
+if((!noodles.as.genes.tables.loaded) && file.exists('noodles.as.genes.tables.Rda'))
+{
+	message('Loading as genes noodles...')
+	loaded<-load('noodles.as.genes.tables.Rda')
+	if (
+		'as.genes.noodles.ranges' %in% loaded &&
+		'mbd.as.genes.noodles.coverage' %in%  loaded &&
+		class(as.genes.noodles.ranges)=='GRanges' &&	
+		(
+			class(mbd.as.genes.noodles.coverage)=='dgCMatrix' || 
+			class(mbd.as.genes.noodles.coverage)=='matrix')
+		)	noodles.as.genes.tables.loaded <-TRUE
+}
+#if we loaded it, we do nothing
+
+if(!noodles.as.genes.tables.loaded)
+{
+	noodlen<-1000
+	expand<-1 #noodles
+	as.genes.noodles.ranges<-unlist(GRangesList(lapply(as.genes.symbols,function(gene) {
+		message(gene)
+		currentgenename<-gene
+		if(gene=='MIR143HG') currentgenename<-'CARMN'
+		if(gene=='LRRC48') currentgenename<-'DRC3'
+		if(gene=='UTP11L') currentgenename<-'UTP11'
+		entrez<-suppressWarnings(select(org.Hs.eg.db,keys=c(currentgenename),columns=c('ENTREZID'),keytype='SYMBOL')$ENTREZID)
+		gene.start<-suppressWarnings(min(select(TxDb.Hsapiens.UCSC.hg19.knownGene,keys=c(entrez),keytype='GENEID',columns=c('TXSTART'))$TXSTART))
+		gene.end<-suppressWarnings(max(select(TxDb.Hsapiens.UCSC.hg19.knownGene,keys=c(entrez),keytype='GENEID',columns=c('TXEND'))$TXEND))
+		gene.chr<-suppressWarnings(select(TxDb.Hsapiens.UCSC.hg19.knownGene,keys=c(entrez),keytype='GENEID',columns=c('TXCHROM'))$TXCHROM)
+		noodstart<-gene.start-(gene.start %% noodlen) - expand*noodlen
+		noodend<-gene.end-(gene.end %% noodlen)+(expand+1)*noodlen
+		gene.noodles<-data.frame('chr'=gene.chr,'start'=seq(noodstart,noodend-noodlen,noodlen),'end'=seq(noodstart+noodlen-1,noodend,noodlen))
+		if(currentgenename!=gene) currentgenename<-paste0(currentgenename,'(',gene,')')
+		rownames(gene.noodles)<-paste0(currentgenename,'@',gene.noodles$chr,":",gene.noodles$start,'-',gene.noodles$end)
+		noodlesGRanges.gene <- GRanges(gene.noodles,seqinfo=nucl.chromosomes.hg19())
+	} )))
+	
+	mbd.as.genes.noodles.coverage<-count.coverage.of.noodles(as.genes.noodles.ranges,paste(mbd.bed.dir,mbd.bed.files,sep='/'),mbd.bed.ids)
+	
+	save(file='noodles.as.genes.tables.Rda',list=c('as.genes.noodles.ranges','mbd.as.genes.noodles.coverage'))
+}
+
