729dae9aeacaea494698873775725c56419eb288
Alexander Favorov <favorov@sensi.org>
2016-10-25 02:54:11 -0400

    started the 100-noodles story for the data

---
+++ DiffMeth/genomewidenoodles/C/prepare.gw.noodles.C.R
@@ -0,0 +1,37 @@
+if (!suppressWarnings(require('differential.coverage')))
+{
+	if (!suppressWarnings(require('devtools')))
+	{
+		source("http://bioconductor.org/biocLite.R")
+		biocLite("devtools")
+		library("devtools")
+	}
+	install_github('favorov/differential.coverage')
+	#load_all('../../../../differential.coverage/')
+	library('differential.coverage')
+}
+
+noodles.C.loaded<-FALSE
+# we can the whole thing to noodles.C.Rda
+if(file.exists('noodles.C.Rda'))
+{
+	loaded<-load('noodles.C.Rda')
+	if ('noodles.C.methylation' %in% loaded) 
+		if (class(noodles.C.methylation)=='data.frame')
+			if ('noodles.C' %in% loaded)
+				if(class(noodles.C)=='GRanges')
+			noodles.C.loaded<-TRUE
+}
+
+if(!noodles.C.loaded)
+{
+	beddir<-'../../../../Methylation/bedfiles/'
+	noodle.length<-100
+	chrs<-nucl.chromosomes.hg19()
+	noodles.C<-prepare.uniform.noodles(chrs,noodle.length)
+	bedfiles<-dir(beddir) 
+	bedfiles<-bedfiles[grep('All_',bedfiles,invert=TRUE)] # remove two 'All_' files
+	bed.ids<-sapply(strsplit(bedfiles,split='_'),function(x){if(x[2]!='DNA') x[2] else x[3]}) #somhere id in pos 2, somewhere in 3
+	noodles.C.methylation<-count.coverage.of.noodles(noodles.C,paste0(beddir,bedfiles),bed.ids)
+	save(file='noodles.C.Rda',list=c('noodles.C','noodles.C.methylation','bed.ids','noodle.length'))
+}
