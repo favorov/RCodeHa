3a8271a8f0ca9b5cdc7fe272046b2a3494788733
Alexander Favorov <favorov@sensi.org>
2017-01-25 04:20:44 +0300

    dubug line to save DM.Genes

--- DiffMeth/genomewidenoodles/C/lists.and.beds.for.C.DM.noodles.R
+++ DiffMeth/genomewidenoodles/C/lists.and.beds.for.C.DM.noodles.R
@@ -81,6 +81,8 @@ message('bonferroni,flanks 100000')
 
 DM.Genes<-genes.with.TSS.covered(noodles=DM.C.noodles,flanks=100000)
 
+save(file='DM.Genes.100000',list=c(DM.genes))
+
 DM.Genes.df<-as(DM.Genes,'data.frame')
 
 DM.Genes.df<-DM.Genes.df[order(DM.Genes.df$seqnames,DM.Genes.df$start),]
